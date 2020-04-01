360d24c9b1642355401a1c6bc07c3e1f00c97362
Eliana88 <eliana.ramos1988@gmail.com>
2020-04-01 19:37:27 -0300

    primer commit

---
+++ src/main/java/com/irso/apialumnos/AppAlumnosIrsoApplication.java
@@ -0,0 +1,13 @@
+package com.irso.apialumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAlumnosIrsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAlumnosIrsoApplication.class, args);
+	}
+
+}
